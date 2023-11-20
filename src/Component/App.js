b631eee4775a3af71167a57457c28d662ac84275
Mwambui123 <maurinawambuia@gmail.com>
2023-11-20 10:29:55 +0300

    fetch data from the server

--- src/Component/App.js
+++ src/Component/App.js
@@ -1,10 +1,22 @@
 
 import './App.css';
+import { useEffect, useState } from 'react';
 
 
 
 
-function App() {
+  function App() {
+
+    const [robots, setRobots]=useState()
+  
+  useEffect(() =>{
+    fetch('http://localhost:3000/bots')
+    .then((res)=>res.json())
+    .then(data =>{
+      setRobots(data)
+    })
+  },[])
+
   return (
     <div className="App">
     
